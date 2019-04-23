f50876701b3e341f0acc6116beee8553fbd3067b
sofianeyra <safirokpoper@gmail.com>
2019-04-22 22:15:45 -0500

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBd95MQtxM0qjsqcAb1b-JEXmmPnJ6Plh4',
+  authDomain: 'burgerqueen-d1101.firebaseapp.com',
+  databaseURL: 'https://burgerqueen-d1101.firebaseio.com',
+  projectId: 'burgerqueen-d1101',
+  storageBucket: 'burgerqueen-d1101.appspot.com',
+  messagingSenderId: '775031170392',
+};
+firebase.initializeApp(config);
+
+export default firebase;
