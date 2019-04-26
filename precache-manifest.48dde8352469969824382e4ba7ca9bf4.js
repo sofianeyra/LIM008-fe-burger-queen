c2c92f4f4f8a27587b3507d443fb53710f574674
sofianeyra <safirokpoper@gmail.com>
2019-04-26 15:49:08 -0500

    Updates

---
+++ precache-manifest.48dde8352469969824382e4ba7ca9bf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a4f816d571dde5ac940",
+    "url": "/LIM008-fe-burger-queen/static/js/runtime~main.10776b7b.js"
+  },
+  {
+    "revision": "a8d93ce7909449551619",
+    "url": "/LIM008-fe-burger-queen/static/js/main.2475147b.chunk.js"
+  },
+  {
+    "revision": "e7a4ecc757b2de65168c",
+    "url": "/LIM008-fe-burger-queen/static/js/2.227d328c.chunk.js"
+  },
+  {
+    "revision": "a8d93ce7909449551619",
+    "url": "/LIM008-fe-burger-queen/static/css/main.19346287.chunk.css"
+  },
+  {
+    "revision": "3efeb4312818ac7713ad063287d40de4",
+    "url": "/LIM008-fe-burger-queen/index.html"
+  }
+];
